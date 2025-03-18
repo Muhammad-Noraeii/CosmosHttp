4a73d1ede71c837181d7461d1d2f2fd8f3a5f20e
Muhammad-Noraeii <Muhammad.Noraeii@gmail.com>
2025-03-18 12:00:39 +0330

    Update HttpRequest.cs
    Added proper constants for default values
    Improved property syntax with C# expression-bodied members
    Restructured the Send method to use more focused helper methods
    Added null-conditional operators for cleaner code

--- source/Client/HttpRequest.cs
+++ source/Client/HttpRequest.cs
@@ -1,6 +1,6 @@
-﻿/*
+/*
 * PROJECT:          CosmosHttp Development
-* CONTENT:          Http Request class (Heavily inspered by https://github.com/2881099/TcpClientHttpRequest)
+* CONTENT:          Http Request class
 * PROGRAMMERS:      Valentin Charbonnier <valentinbreiz@gmail.com>
 */
 
@@ -15,35 +15,35 @@ namespace CosmosHttp.Client
 {
     public class HttpRequest : HttpPacket
     {
+        private const int DEFAULT_TIMEOUT = 20000;
+        private const string DEFAULT_USER_AGENT = "CosmosHttp Client (CosmosOS)";
+        
         private TcpClient _client;
         private string _remote;
         private string _path;
-        private int _timeout = 20000;
+        private int _timeout = DEFAULT_TIMEOUT;
         private NetworkStream _stream;
         private HttpResponse _response;
 
         public string Path
         {
-            get
-            {
-                return _path;
-            }
-            set
-            {
-                _path = value;
-            }
+            get => _path;
+            set => _path = value;
         }
 
-        public HttpResponse Response
+        public HttpResponse Response => _response;
+
+        public int Timeout
         {
-            get { return _response; }
+            get => _timeout;
+            set => _timeout = value;
         }
 
         public HttpRequest()
         {
             _headers.Add("Connection", "Keep-Alive");
             _headers.Add("Accept", "*/*");
-            _headers.Add("User-Agent", "CosmosHttp Client (CosmosOS)");
+            _headers.Add("User-Agent", DEFAULT_USER_AGENT);
             _headers.Add("Accept-Language", "en-us");
             _headers.Add("Accept-Encoding", "gzip, deflate");
         }
@@ -52,10 +52,7 @@ namespace CosmosHttp.Client
         {
             if (_client != null)
             {
-                if (_stream != null)
-                {
-                    _stream.Close();
-                }
+                _stream?.Close();
                 _client.Close();
                 _client = null;
             }
@@ -78,10 +75,10 @@ namespace CosmosHttp.Client
             _headers.Remove("Content-Length");
             if (!string.IsNullOrEmpty(data) && string.Compare(_method, "post", true) == 0)
             {
-                _headers["Content-Length"] = string.Concat(Encoding.ASCII.GetBytes(data).Length);
+                _headers["Content-Length"] = Encoding.ASCII.GetBytes(data).Length.ToString();
                 if (string.IsNullOrEmpty(_headers["Content-Type"]))
                 {
-                    _headers["Content-Type"] = "application/x-www-form-urlencoded; charset=" + _charset;
+                    _headers["Content-Type"] = $"application/x-www-form-urlencoded; charset={_charset}";
                 }
                 else if (_headers["Content-Type"].IndexOf("multipart/form-data") == -1)
                 {
@@ -91,28 +88,42 @@ namespace CosmosHttp.Client
                     }
                     if (_headers["Content-Type"].IndexOf("charset=") == -1)
                     {
-                        _headers["Content-Type"] += "; charset=" + _charset;
+                        _headers["Content-Type"] += $"; charset={_charset}";
                     }
                 }
                 data += "\r\n\r\n";
             }
             _headers["Host"] = _domain;
 
-            string http = _method + " " + _path + " HTTP/1.1\r\n";
-            foreach (string head in _headers.Keys)
+            StringBuilder httpBuilder = new StringBuilder();
+            httpBuilder.AppendFormat("{0} {1} HTTP/1.1\r\n", _method, _path);
+            
+            foreach (var header in _headers)
             {
-                http += head + ": " + _headers[head] + "\r\n";
+                httpBuilder.AppendFormat("{0}: {1}\r\n", header.Key, header.Value);
             }
 
-            http += "\r\n" + data;
-            _head = http;
-            byte[] request = Encoding.ASCII.GetBytes(http);
+            httpBuilder.AppendFormat("\r\n{0}", data);
+            _head = httpBuilder.ToString();
+            byte[] request = Encoding.ASCII.GetBytes(_head);
+            
+            EstablishConnection();
+            SendRequest(request);
+            receive(_stream, redirections, _ip);
+        }
+
+        private void EstablishConnection()
+        {
             if (_client == null || _remote == null)
             {
                 _remote = _ip;
-                this.Close();
+                Close();
                 _client = new TcpClient(_ip, 80);
             }
+        }
+
+        private void SendRequest(byte[] request)
+        {
             try
             {
                 _stream = getStream();
@@ -120,17 +131,15 @@ namespace CosmosHttp.Client
             }
             catch
             {
-                this.Close();
+                Close();
                 _client = new TcpClient(_ip, 80);
                 _stream = getStream();
                 _stream.Write(request, 0, request.Length);
             }
-            receive(_stream, redirections, _ip);
         }
 
         protected void receive(Stream stream, int redirections, string action)
         {
-            // stream.ReadTimeout = _timeout; TO PLUG
             _response = null;
             byte[] bytes = new byte[1024];
             int bytesRead = 0;
@@ -140,7 +149,6 @@ namespace CosmosHttp.Client
 
             while (true)
             {
-                int idx = -1;
                 try
                 {
                     bytesRead = stream.Read(bytes, 0, bytes.Length);
@@ -161,84 +169,89 @@ namespace CosmosHttp.Client
 
                 if (_response == null)
                 {
-                    // Add the newly read bytes to the head buffer
-                    int oldLength = headBuffer != null ? headBuffer.Length : 0;
-                    byte[] newHeadBuffer = new byte[oldLength + bytesRead];
-                    if (headBuffer != null)
+                    headBuffer = AppendToBuffer(headBuffer, bytes, bytesRead);
+                    int headerDelimiterIndex = Utils.findBytes(headBuffer, new byte[] { 13, 10, 13, 10 }, 0);
+                    
+                    if (headerDelimiterIndex != -1)
                     {
-                        Array.Copy(headBuffer, 0, newHeadBuffer, 0, oldLength);
-                    }
-                    Array.Copy(bytes, 0, newHeadBuffer, oldLength, bytesRead);
-                    headBuffer = newHeadBuffer;
-
-                    // Check for the header delimiter
-                    idx = Utils.findBytes(headBuffer, new byte[] { 13, 10, 13, 10 }, 0);
-                    if (idx != -1)
-                    {
-                        // Create the response with the header
-                        byte[] header = new byte[idx];
-                        Array.Copy(headBuffer, 0, header, 0, idx);
-                        _response = new HttpResponse(this, header);
-                        _response.Received += headBuffer.Length - idx - 4;
-
-                        // Transfer remaining bytes to the body buffer
-                        int bodyLength = headBuffer.Length - idx - 4;
+                        CreateResponseFromHeader(headBuffer, headerDelimiterIndex);
+                        
+                        int bodyLength = headBuffer.Length - headerDelimiterIndex - 4;
                         bodyBuffer = new byte[bodyLength];
-                        Array.Copy(headBuffer, idx + 4, bodyBuffer, 0, bodyLength);
+                        Array.Copy(headBuffer, headerDelimiterIndex + 4, bodyBuffer, 0, bodyLength);
                     }
                 }
                 else
                 {
                     _response.Received += bytesRead;
-                    // Add the newly read bytes to the body buffer
-                    int oldLength = bodyBuffer != null ? bodyBuffer.Length : 0;
-                    byte[] newBodyBuffer = new byte[oldLength + bytesRead];
-                    if (bodyBuffer != null)
-                    {
-                        Array.Copy(bodyBuffer, 0, newBodyBuffer, 0, oldLength);
-                    }
-                    Array.Copy(bytes, 0, newBodyBuffer, oldLength, bytesRead);
-                    bodyBuffer = newBodyBuffer;
+                    bodyBuffer = AppendToBuffer(bodyBuffer, bytes, bytesRead);
                 }
 
-                if (_response != null)
+                if (_response != null && _response.ContentLength >= 0 && _response.ContentLength <= bodyBuffer.Length)
                 {
-                    if (_response.ContentLength >= 0)
-                    {
-                        if (_response.ContentLength <= bodyBuffer.Length)
-                        {
-                            break;
-                        }
-                    }
+                    break;
                 }
             }
 
+            HandleResponseCompletion(bodyBuffer, exception);
+        }
+
+        private byte[] AppendToBuffer(byte[] existingBuffer, byte[] newData, int bytesRead)
+        {
+            int oldLength = existingBuffer != null ? existingBuffer.Length : 0;
+            byte[] newBuffer = new byte[oldLength + bytesRead];
+            
+            if (existingBuffer != null)
+            {
+                Array.Copy(existingBuffer, 0, newBuffer, 0, oldLength);
+            }
+            
+            Array.Copy(newData, 0, newBuffer, oldLength, bytesRead);
+            return newBuffer;
+        }
+
+        private void CreateResponseFromHeader(byte[] headBuffer, int headerDelimiterIndex)
+        {
+            byte[] header = new byte[headerDelimiterIndex];
+            Array.Copy(headBuffer, 0, header, 0, headerDelimiterIndex);
+            _response = new HttpResponse(this, header);
+            _response.Received += headBuffer.Length - headerDelimiterIndex - 4;
+        }
+
+        private void HandleResponseCompletion(byte[] bodyBuffer, Exception exception)
+        {
             if (_response == null)
             {
                 this.closeTcp();
+                ThrowWebException(exception);
+            }
+            else
+            {
+                _response.SetStream(bodyBuffer);
+                this.closeTcp();
+            }
+        }
 
-                // Construct the request headers string
-                List<string> sb = new List<string>();
-                sb.Add(_method.ToUpper() + " " + _ip + " HTTP/1.1");
-                foreach (string header in _headers.Keys)
-                {
-                    sb.Add(header + ": " + _headers[header]);
-                }
-
-                // Throw a WebException with the appropriate message
-                if (exception == null)
-                {
-                    throw new WebException("WebException " + string.Join("\r\n", sb.ToArray()));
-                }
-                else
-                {
-                    throw new WebException(exception.Message + "\r\n" + string.Join("\r\n", sb.ToArray()), exception);
-                }
+        private void ThrowWebException(Exception exception)
+        {
+            List<string> requestHeaders = new List<string>();
+            requestHeaders.Add($"{_method.ToUpper()} {_ip} HTTP/1.1");
+            
+            foreach (var header in _headers)
+            {
+                requestHeaders.Add($"{header.Key}: {header.Value}");
             }
 
-            _response.SetStream(bodyBuffer);
-
-            this.closeTcp();
+            string requestHeadersStr = string.Join("\r\n", requestHeaders.ToArray());
+            
+            if (exception == null)
+            {
+                throw new WebException($"WebException {requestHeadersStr}");
+            }
+            else
+            {
+                throw new WebException($"{exception.Message}\r\n{requestHeadersStr}", exception);
+            }
         }
 
         protected bool closeTcp()
@@ -257,17 +270,18 @@ namespace CosmosHttp.Client
     {
         public static int findBytes(byte[] source, byte[] find, int startIndex)
         {
-            if (find == null) return -1;
-            if (find.Length == 0) return -1;
-            if (source == null) return -1;
-            if (source.Length == 0) return -1;
-            if (startIndex < 0) startIndex = 0;
+            if (find == null || find.Length == 0 || source == null || source.Length == 0)
+                return -1;
+                
+            if (startIndex < 0) 
+                startIndex = 0;
+                
             int idx = -1, idx2 = startIndex - 1;
+            
             do
             {
-                idx2 = idx = Array.FindIndex<byte>(source, Math.Min(idx2 + 1, source.Length), delegate (byte b) {
-                    return b == find[0];
-                });
+                idx2 = idx = Array.FindIndex<byte>(source, Math.Min(idx2 + 1, source.Length), b => b == find[0]);
+                
                 if (idx2 != -1)
                 {
                     for (int a = 1; a < find.Length; a++)
@@ -281,7 +295,8 @@ namespace CosmosHttp.Client
                     if (idx != -1) break;
                 }
             } while (idx2 != -1);
+            
             return idx;
         }
     }
-}
+} 
